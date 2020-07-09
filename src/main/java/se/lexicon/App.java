6b1f7c9640964b8d8ad270c2765e21654876fd28
Mattias Andersson <neonithe@gmail.com>
2020-07-09 10:16:44 +0200

    First step_refactor_nr2

--- src/main/java/se/lexicon/App.java
+++ src/main/java/se/lexicon/App.java
@@ -1,15 +1,19 @@
 package se.lexicon;
 
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import se.lexicon.config.ComponentScanConfig;
 import se.lexicon.data_access.StudentDAO;
+import se.lexicon.models.Student;
 
 public class App 
 {
     public static void main( String[] args )
     {
         AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(ComponentScanConfig);
+                new AnnotationConfigApplicationContext(ComponentScanConfig.class);
 
         StudentDAO studentDAO = context.getBean(StudentDAO.class);
+
+        System.out.println(new Student("Mattias"));
     }
 }
--- src/main/java/se/lexicon/data_access/StudentDAOListImpl.java
+++ src/main/java/se/lexicon/data_access/StudentDAOListImpl.java
@@ -3,27 +3,40 @@ package se.lexicon.data_access;
 import org.springframework.stereotype.Component;
 import se.lexicon.models.Student;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Component
 public class StudentDAOListImpl implements StudentDAO{
+
+    private List<Student> studentList = new ArrayList<>();
+
     @Override
     public Student save(Student student) {
-        return null;
+        if(studentList.contains(student)){
+            studentList.add(student);
+        }
+        return student;
     }
 
     @Override
     public Student find(int id) {
-        return null;
+        for(Student student : studentList){
+            if(student.getId() == id){
+                return student;
+            }
+        }
+        throw new RuntimeException("Cannot find"+id);
     }
 
     @Override
     public List<Student> findAll() {
-        return null;
+        return studentList;
     }
 
     @Override
     public void delete(int id) {
-
+        Student delete = find(id);
+        studentList.remove(delete);
     }
 }
